f08096767ca8949f8a2e1a6f004a242a4d140ab2
Anthony Grimes <i@raynes.me>
2012-11-30 22:54:59 -0600

    Add watching for directory creation.

--- src/filevents/core.clj
+++ src/filevents/core.clj
@@ -1,9 +1,14 @@
 (ns filevents.core
-  (:require [fs.core :as fs]))
+  (:require [fs.core :as fs]
+            [clojure.set :refer [difference]]))
 
-(def ^:dynamic *check-interval*
+(def ^:dynamic *check-file-interval*
   "Amount of time between checks for modifications."
   300)
+
+(def ^:dynamic *check-dir-interval*
+  "Amount of time between checking a directory for new files."
+  1000)
 
 (defn watch-file
   "Set up a thread that watches a file for two events: deletion
@@ -13,7 +18,7 @@
   [file f]
   (future
     (loop [time (fs/mod-time file)]
-      (Thread/sleep *check-interval*)
+      (Thread/sleep *check-file-interval*)
       (let [new-time (fs/mod-time file)]
         (cond
          (not (fs/exists? file)) (f :deleted file)
@@ -21,14 +26,35 @@
                                      (recur new-time))
          :else                   (recur new-time))))))
 
+(defn ^:private get-file-set
+  "Get a set of non-directory files in a directory recursively."
+  [dir]
+  (->> (fs/file dir)
+       (file-seq)
+       (filter fs/file?)
+       (set)))
+
+(defn watch-dir
+  "Recursively watch all the files in a directory and watch for
+   new files."
+  [dir f]
+  (let [files (get-file-set dir)]
+    (doseq [file files]
+      (watch-file file f))
+    (future
+      (loop [old files]
+        (Thread/sleep *check-dir-interval*)
+        (let [files (get-file-set dir)
+              new-files (difference files old)]
+          (doseq [file new-files]
+            (future (f :created file)))
+          (recur files))))))
+
 (defn watch
   "Set up watcher threads on files. If any directories are included,
    recursively sets up watchers on the files contained in them."
   [f & files]
-  (doall
-   (flatten
-    (for [file files]
-      (if (fs/directory? file)
-        (map #(watch-file % f)
-             (filter fs/file? (file-seq (fs/file file))))
-        (watch-file file f))))))
+  (doseq [file files]
+    (if (fs/directory? file)
+      (watch-dir file f)
+      (watch-file file f))))
