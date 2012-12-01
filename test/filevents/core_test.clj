7927b3b3ee8bf147b44b5b3918f0dbcfc49ac10e
Anthony Grimes <i@raynes.me>
2012-12-01 04:31:14 -0600

    Write tests.

--- test/filevents/core_test.clj
+++ test/filevents/core_test.clj
@@ -1,7 +1,24 @@
 (ns filevents.core-test
   (:use clojure.test
-        filevents.core))
+        filevents.core)
+  (:require [fs.core :as fs]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest watch-test 
+  (let [check (atom nil)]
+    (watch (fn [kind _] (swap! check (constantly kind))) "test/testdir2/bar" "test/testdir")
+    (testing "Watches for created files"
+      (fs/touch "test/testdir/foo")
+      (Thread/sleep 1500)
+      (is (= :created @check)))
+    (testing "Watches for modified files"
+      (fs/touch "test/testdir/foo")
+      (Thread/sleep 1000)
+      (is (= :modified @check)))
+    (testing "Watches for deleted files"
+      (fs/delete "test/testdir/foo")
+      (Thread/sleep 1000)
+      (is (= :deleted @check)))
+    (testing "Works for single files"
+      (fs/touch "test/testdir2/bar")
+      (Thread/sleep 1000)
+      (is (= :modified @check)))))
